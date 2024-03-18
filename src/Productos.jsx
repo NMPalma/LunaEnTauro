1f9a8ab6bf121b26756e62bab0e26923b7949674
“NMPalma” <palma_nicolas94@hotmail.com>
2024-03-18 20:23:49 -0300

    Agregando sidebar

--- src/Productos.jsx
+++ src/Productos.jsx
@@ -1,10 +1,57 @@
 import Header from "./components/Header";
+import { useState } from "react";
 
 export function Productos() {
+    const [mostrarMarcas, setMostrarMarcas] = useState(false);
+    const [mostrarProductos, setMostrarProductos] = useState(false);
+    const [tituloActivo, setTituloActivo] = useState(null);
+
+    const toggleMarcas = () => {
+        setMostrarMarcas(!mostrarMarcas);
+        if (mostrarProductos) {
+            setMostrarProductos(false);
+        }
+        setTituloActivo('marcas');
+    };
+
+    const toggleProductos = () => {
+        setMostrarProductos(!mostrarProductos);
+        if (mostrarMarcas) {
+            setMostrarMarcas(false);
+        }
+        setTituloActivo('productos');
+    };
     return (
         <div>
             <Header />
-            <h1>Aca van los productos</h1>
+            <div className="productos-container">
+                <div className="sidebar">
+                    <h2
+                        onClick={toggleMarcas}
+                        className={tituloActivo === 'marcas' ? 'activo' : ''}
+                    >
+                        Marcas
+                    </h2>
+                    <ul className={mostrarMarcas ? 'mostrar' : ''}>
+                        <li>Marca 1</li>
+                        <li>Marca 2</li>
+                        <li>Marca 3</li>
+                        {/* Agrega más marcas según sea necesario */}
+                    </ul>
+                    <h2
+                        onClick={toggleProductos}
+                        className={tituloActivo === 'productos' ? 'activo' : ''}
+                    >
+                        Productos
+                    </h2>
+                    <ul className={mostrarProductos ? 'mostrar' : ''}>
+                        <li>Producto 1</li>
+                        <li>Producto 2</li>
+                        <li>Producto 3</li>
+                        {/* Agrega más productos según sea necesario */}
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,15 @@
 import '/src/index.css'
 import Header from "./Header"
+import React from 'react'
 
 export function Home() {
     return (
-        <Header />
+        <React.StrictMode>
+            <Header />
+            <div className='container'>
+                <img className='logo' src="/src/images/logosinfondo.png" alt="Logo Luna En Tauro" />
+                <h1>Bienvenidos a Luna en Tauro</h1>
+            </div>
+        </React.StrictMode >
     )
 }
